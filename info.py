821639304276426c76f2cfd4733df464238f08ec
l28440965-cloud <l28440965@gmail.com>
2026-02-07 12:02:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -55,7 +55,7 @@ SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/')  # Support group lin
 
 # FORCE_SUB 
 auth_req_channels = environ.get("AUTH_REQ_CHANNELS","-1003876805082")# requst to join Channel for force sub (make sure bot is admin) only for bot ADMINS  
-auth_channels     = environ.get("AUTH_CHANNELS", "-1003815684659")# Channels for force sub (make sure bot is admin)
+auth_channels     = environ.get("AUTH_CHANNELS", "-1003679641529")# Channels for force sub (make sure bot is admin)
 
 # ============================
 # Payment Configuration
