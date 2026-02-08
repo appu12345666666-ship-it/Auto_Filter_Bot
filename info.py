a2d381d5780f76ac899a8a80650da1f5caf47d85
messi21122255-creator <messi21122255@gmail.com>
2026-02-08 11:20:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -54,7 +54,7 @@ reqst_channel = environ.get('REQST_CHANNEL_ID', '-1003400481441')  # Request cha
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/')  # Support group link (make sure bot is admin)
 
 # FORCE_SUB 
-auth_req_channels = environ.get("AUTH_REQ_CHANNELS","-1003876805082 1002946611673")# requst to join Channel for force sub (make sure bot is admin) only for bot ADMINS  
+auth_req_channels = environ.get("AUTH_REQ_CHANNELS","-1003876805082")# requst to join Channel for force sub (make sure bot is admin) only for bot ADMINS  
 auth_channels     = environ.get("AUTH_CHANNELS", "-1003679641529")# Channels for force sub (make sure bot is admin)
 
 # ============================
